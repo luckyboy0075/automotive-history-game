d06a4f8508fc0590c065d347dfd4f53a53038bde
prn.m_holovan <prn.m_holovan@wargaming.net>
2025-01-28 20:10:54 +0100

    Update 5

---
+++ js/media.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('./content/media.json')
+        .then(response => response.json())
+        .then(data => {
+            const mediaGallery = document.querySelector(".media-gallery");
+
+            data.forEach(media => {
+                const mediaItem = document.createElement("div");
+                mediaItem.className = "media-item";
+
+                if (media.media_type === "Image") {
+                    mediaItem.innerHTML = `<img src="${media.media_url}" alt="${media.media_title}">`;
+                } else if (media.media_type === "Video") {
+                    mediaItem.innerHTML = `<video controls><source src="${media.media_url}" type="video/mp4">Your browser does not support videos.</video>`;
+                }
+
+                mediaGallery.appendChild(mediaItem);
+            });
+        })
+        .catch(error => console.error("Error loading media:", error));
+});
