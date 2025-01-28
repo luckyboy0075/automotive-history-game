3a2a5956b315f94e70c484f9c5558a7d83d871f9
prn.m_holovan <prn.m_holovan@wargaming.net>
2025-01-28 23:03:54 +0100

    Update 16

--- js/goals.js
+++ js/goals.js
@@ -1,16 +1,15 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     fetch('./content/goals.json')
-        .then(response => response.json())
-        .then(data => {
-            const completed = data.filter(goal => goal.status === "Completed").length;
-            const total = data.length;
+        .then((response) => response.json())
+        .then((goalsData) => {
+            const completed = goalsData.filter(goal => goal.status === "Completed").length;
+            const total = goalsData.length;
             const progressBar = document.getElementById("progress-bar");
             const progressText = document.getElementById("progress-text");
-
             const percentage = (completed / total) * 100;
-            progressBar.style.width = percentage + "%";
+            progressBar.style.width = `${percentage}%`;
             progressText.textContent = `Progress: ${completed} of ${total} goals completed (${Math.round(percentage)}%)`;
         })
-        .catch(error => console.error("Error loading goals:", error));
+        .catch((error) => console.error("Goals Loading Error:", error));
 });
