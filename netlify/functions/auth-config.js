d14de7a69cb8c1f8639100eaf4eec92bf60dba76
prn.m_holovan <prn.m_holovan@wargaming.net>
2025-03-11 08:57:12 +0100

    Fixes

---
+++ netlify/functions/auth-config.js
@@ -0,0 +1,10 @@
+exports.handler = async function () {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+            AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+            REDIRECT_URI: "https://your-site.netlify.app/admin/"
+        }),
+    };
+};
