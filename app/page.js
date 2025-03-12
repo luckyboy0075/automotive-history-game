dd59d8a573e70fbc53e8114622767824766f350e
prn.m_holovan <prn.m_holovan@wargaming.net>
2025-03-12 18:47:02 +0100

    Fixing CMS, perfrorming Neltify compatible changes 1

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <main>
-            <h1>Welcome to the Automotive History Game!</h1>
-            <p>Explore the history of automotive engineering through interactive storytelling.</p>
-        </main>
-    );
-}
