107d3873c4d1130a212e724afda4d49156ba477b
prn.m_holovan <prn.m_holovan@wargaming.net>
2025-03-13 14:15:26 +0100

    Fixing CMS, perfrorming Neltify compatible changes 7

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -5,11 +5,25 @@ exports.handler = async () => {
             "Content-Type": "text/yaml"
         },
         body: `
-        backend:
-          name: auth0
-          client_id: "${process.env.AUTH0_CLIENT_ID}"
-          domain: "${process.env.AUTH0_DOMAIN}"
-          redirect_uri: "https://petrolheadsgame.com/admin/"
-        `
+backend:
+  name: "auth0"
+  client_id: "${process.env.AUTH0_CLIENT_ID}"
+  domain: "${process.env.AUTH0_DOMAIN}"
+  redirect_uri: "https://petrolheadsgame.com/admin/"
+
+media_folder: "uploads"
+public_folder: "/uploads"
+
+collections:
+  - name: "posts"
+    label: "Posts"
+    folder: "content/posts"
+    create: true
+    fields:
+      - { name: "title", label: "Title", widget: "string" }
+      - { name: "body", label: "Body", widget: "markdown" }
+      - { name: "date", label: "Date", widget: "datetime" }
+      - { name: "image", label: "Image", widget: "image" }
+`
     };
 };
