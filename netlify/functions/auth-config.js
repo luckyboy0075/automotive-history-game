9d6a21b58f18f5111960079ab130b196fd9fff72
prn.m_holovan <prn.m_holovan@wargaming.net>
2025-03-13 10:34:00 +0100

    Fixing CMS, perfrorming Neltify compatible changes 6

--- netlify/functions/auth-config.js
+++ netlify/functions/auth-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  auth0Domain: process.env.AUTH0_DOMAIN || "petrolheadsgame.eu.auth0.com",
-  auth0ClientId: process.env.AUTH0_CLIENT_ID || "wtqqTxmzjj1CyFm6CpbH1hAtAIfmcwm8",
-  auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET || "OcOZYsMZg84UyGgGwuOdukNT9ktEUwe5HaEBsu61M64FHISvxDxy3XruGF8IL4ph",
-  auth0CallbackUrl: process.env.AUTH0_CALLBACK_URL || "https://petrolheadsgame.com/admin/",
-  auth0IssuerBaseUrl: process.env.AUTH0_ISSUER_BASE_URL || "https://petrolheadsgame.eu.auth0.com/"
+  auth0Domain: process.env.AUTH0_DOMAIN,
+  auth0ClientId: process.env.AUTH0_CLIENT_ID,
+  auth0ClientSecret: process.env.AUTH0_CLIENT_SECRET,
+  auth0CallbackUrl: process.env.AUTH0_CALLBACK_URL,
+  auth0IssuerBaseUrl: process.env.AUTH0_ISSUER_BASE_URL
 };
+
 
 exports.handler = async function () {
     return {
